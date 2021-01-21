be8a5846d9d11e6d2120c35d4149e17e0528f13a
sachin <sachingurjar6698@gmail.com>
2021-01-21 17:44:22 +0530

    saved

--- credits/admin.py
+++ credits/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import User
+from .models import User,Record
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['name', 'current_credits', 'mail_id', 'Phone_num']
+
+admin.site.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ['Name_Of_Sender', 'Name_Of_Receiver', 'Transfer_Amount', 'Sender_Updated_Balance', 'Receiver_Updated_Balance']
